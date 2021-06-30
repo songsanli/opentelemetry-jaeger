2c8ba7948bfc1f3377a73f9b7858772dd0b030f5
Samuel Song <samuel.song@sap.com>
2021-06-30 18:05:29 +0800

    Change SimpleSpanProcessor to BatchSpanProcessor

--- tracing.js
+++ tracing.js
@@ -1,13 +1,13 @@
 "use strict";
 
 const { NodeTracerProvider } = require("@opentelemetry/node");
-const { SimpleSpanProcessor } = require("@opentelemetry/tracing");
+const { BatchSpanProcessor } = require("@opentelemetry/tracing");
 const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 
 const provider = new NodeTracerProvider();
-provider.addSpanProcessor(new SimpleSpanProcessor(new JaegerExporter()));
+provider.addSpanProcessor(new BatchSpanProcessor(new JaegerExporter()));
 provider.register();
 
 registerInstrumentations({
